9a55d191f6389d19a3985bbfe6ce26f797a06fde
Gabriel Ferreira <99598124+GABSVN@users.noreply.github.com>
2022-05-10 01:12:08 +0200

    commits
    Collabos

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-  let card = document.getElementById("card")
-  let nameB = ""
-  let imageB = ""
-  let taglineB = ""
-  let description = ""
-  async function getBeers() {
-  try {
-    const response = await axios.get('https://api.punkapi.com/v2/beers')
-    for (const element of response.data) {
-        nameB = element.name
-        imageB = element.image_url
-        taglineB = element.tagline
-        description = element.description
-        card.innerHTML += `
-                    
-      <div class="card" style="width: 18rem; g-col-3 p-2 m-2">
-            <img src="`+imageB+`" class="card-img-top" alt="...">
-        <div class="`+nameB+`">
-            <p class="card-text">`+taglineB+`</p>
-            <p> `+description+`</p>
-        </div>
-      </div>    `
-   }
-  } catch (error) {
-  }
+let card = document.getElementById("card")
+let nameB = ""
+let imageB = ""
+let taglineB = ""
+let description = ""
+async function getBeers() 
+{
+  try 
+  {
+      const response = await axios.get('https://api.punkapi.com/v2/beers')
+      for (const element of response.data) 
+      {
+          nameB = element.name
+          imageB = element.image_url
+          taglineB = element.tagline
+          description = element.description
+          card.innerHTML += `
+                      
+        <div class="card" style="width: 18rem; g-col-3 p-2 m-2">
+              <img src="`+imageB+`" class="card-img-top" alt="...">
+          <div class="`+nameB+`">
+              <p class="card-text">`+taglineB+`</p>
+              <p> `+description+`</p>
+          </div>
+        </div>    `
+      }
+  } catch (error) 
+    {
+    }
 }
 
 await getBeers()
